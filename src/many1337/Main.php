087b4bc79b38dcc504dcd128b313a2648bd8b0dc
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2019-11-22 20:28:48 -0500

    Update Main.php

--- src/many1337/Main.php
+++ src/many1337/Main.php
@@ -75,8 +75,10 @@ class Main extends PluginBase implements Listener
         $player->getInventory()->setItem(6, Item::get(280)->setCustomName(TextFormat::GREEN . "SellInv"));
 
     }
-
-        }
+    public function onInteract(PlayerInteractEvent $event)
+    {
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
 
         if ($item->getCustomName() == TextFormat::BLUE . "TutorialUI") {
             $api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
