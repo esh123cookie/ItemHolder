6086eb1ce69d634b11db4b64706918e2cfb77646
test474 <57404055+test474@users.noreply.github.com>
2019-11-22 21:48:55 -0500

    Update Main.php

--- src/many1337/Main.php
+++ src/many1337/Main.php
@@ -76,6 +76,8 @@ class Main extends PluginBase implements Listener
     }
     public function onInteract(PlayerInteractEvent $event)
     {
+        $player = $event->getPlayer();
+        $item = $player->getInventory()->getItemInHand();
         }
 	
         if ($item->getCustomName() == TextFormat::BLUE . "TutorialUI") {
