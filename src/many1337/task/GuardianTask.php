94b53baf3f136e6ab23038c832e9c4fb4d21737e
esh123cookie <52058925+esh123cookie@users.noreply.github.com>
2019-11-22 19:59:54 -0500

    Create GuardianTask.php

---
+++ src/many1337/task/GuardianTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace many1337\task;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\network\mcpe\protocol\LevelEventPacket;
+use many1337\Main;
+
+class GuardianTask extends Task {
+	
+	private $player;
+	private $plugin;
+	
+	public function __construct(Main $plugin, Player $player){
+        $this->plugin = $plugin;
+	$this->player = $player;
+	}
+	
+	public function onRun(int $currentTick){
+		$pk = new LevelEventPacket();
+		$pk->evid = LevelEventPacket::EVENT_GUARDIAN_CURSE;
+		$pk->data = 0;
+		$pk->position = $this->player->asVector3();
+		$this->player->dataPacket($pk);
+	}
+}
